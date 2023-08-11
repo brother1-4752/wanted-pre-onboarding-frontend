ec42ba36fd12eff0fb9676619a994fe7f54d8a6b
brother1-4752 <guddlf4752@gmail.com>
2023-08-11 09:07:07 +0900

    feat: reset css적용

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/Router";
 
+import "./styles/reset.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
