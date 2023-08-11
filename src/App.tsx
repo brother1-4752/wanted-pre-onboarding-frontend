869acbcc3161db8f928b44943c3b416195713454
brother1-4752 <guddlf4752@gmail.com>
2023-08-11 09:33:29 +0900

    debug: 토큰 없는 상태에서 라우팅 오류 해결
    - 토큰 없는 상태에서 /todo뿐만 아니라 홈, 회원가입에 접근해도 모두 로그인 화면으로 강제 전환되는 오류있었음
    - /todo라우팅 시에만 /signin으로 리다이렉트되도록 오류 해결함

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import Layout from './components/common/Layout';
 import hasAuth from './utils/hasAuth';
 import { useCallback, useEffect } from 'react';
 
 function App() {
   const navigate = useNavigate();
+  const location = useLocation();
+
   const callback = useCallback(() => {
     if (hasAuth()) {
       navigate('/todo');
       return <></>;
-    } else {
+    } else if (!hasAuth() && location.pathname === '/todo') {
       navigate('/signin');
       return <></>;
     }
-  }, [navigate]);
+  }, [navigate, location.pathname]);
 
   useEffect(() => {
     callback();
