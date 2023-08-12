565916edad770305382721c0abba87d783faa2ce
brother1-4752 <guddlf4752@gmail.com>
2023-08-12 23:51:46 +0900

    style: TodoContainer 스타일 조정 / feat: api와 get,createTodo 연결
    - createTodos, getTodos api 연결
    - TodoContainer width, height, align-items 프로퍼티 조정

--- src/pages/todo/ToDo.tsx
+++ src/pages/todo/ToDo.tsx
@@ -1,17 +1,68 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import axios from 'axios';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-function ToDo() {
-  const [todoList, setTodoList] = useState<string[]>([]);
-  const [todo, setTodo] = useState<string>('');
-  const [isDone, setIsDone] = useState<boolean>(false);
+interface ITodo {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
 
-  const handleToDoSubmit = (e: FormEvent<HTMLFormElement>) => {
+function ToDo() {
+  const loadedTodoList = () => {
+    const storedTodos = localStorage.getItem('todoList');
+    return storedTodos ? JSON.parse(storedTodos) : [];
+  };
+
+  const [todoList, setTodoList] = useState<ITodo[]>(loadedTodoList);
+  const [todo, setTodo] = useState<string>('');
+
+  // const [isDone, setIsDone] = useState<boolean>(false);
+  const token = localStorage.getItem('access_token');
+
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const response = await axios.get(
+          'https://www.pre-onboarding-selection-task.shop/todos',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (response.status === 200) {
+          console.log(response.data);
+          setTodoList(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getTodos();
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }, []);
+
+  const handleToDoSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    setTodoList((prev) => [...prev, todo]);
-    console.log(todoList);
-    setTodo('');
+    try {
+      const response = await axios.post(
+        'https://www.pre-onboarding-selection-task.shop/todos',
+        { todo },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(response.data);
+      setTodo('')
+    } catch (error: any) {
+      alert(error.response.data.message[0]);
+    }
   };
 
   const handleChangeByTodo = (e: ChangeEvent<HTMLInputElement>) => {
@@ -19,8 +70,24 @@ function ToDo() {
     console.log(todo);
   };
 
+  const handleChageByCheckbox = (i: number) => {
+    // setIsDone((prev) => !prev);
+    const a = todoList.map((el, index) =>
+      index === i
+        ? {
+            id: el.id,
+            isCompleted: !el.isCompleted,
+            todo: el.todo,
+            userId: el.userId,
+          }
+        : el
+    );
+    setTodoList(a);
+  };
+
   return (
     <StyledTodoContainer>
+      <h1>투두리스트</h1>
       <form onSubmit={handleToDoSubmit}>
         <input
           type="text"
@@ -33,41 +100,32 @@ function ToDo() {
         </button>
       </form>
 
-      {todoList.map((v) => (
-        <li key={v}>
-          <label>
-            <input
-              type="checkbox"
-              onClick={(e) => setIsDone((prev) => !prev)}
-            />
-            <span>{v}</span>
-          </label>
-          <button data-testid="modify-button">수정</button>
-          <button data-testid="delete-button">삭제</button>
-        </li>
-      ))}
-
-      {/* <ul>
-        <li>
-          <label>
-            <input type="checkbox" />
-            <span>TODO 1</span>
-          </label>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            <span>TODO 2</span>
-          </label>
-        </li>
-      </ul> */}
+      <ul>
+        {todoList.map((el, index) => (
+          <li key={el.id}>
+            <label>
+              <input
+                type="checkbox"
+                onChange={() => handleChageByCheckbox(index)}
+              />
+              <span>{el.todo}</span>
+            </label>
+            <button data-testid="modify-button">수정</button>
+            <button data-testid="delete-button">삭제</button>
+          </li>
+        ))}
+      </ul>
     </StyledTodoContainer>
   );
 }
 
 const StyledTodoContainer = styled.div`
+  width: 300px;
+  height: 80%;
+
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 export default ToDo;
